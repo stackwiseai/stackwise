89c22b7b99a2f7591685026e0135a8e081452f54
Merwane Hamadi <merwanehamadi@gmail.com>
2023-11-30 14:47:16 -0500

    correct streamingGPT4

--- stacks/stacks/streamingGpt4QuestionAnswerTTS.test.ts
+++ stacks/stacks/streamingGpt4QuestionAnswerTTS.test.ts
@@ -1,2 +1,9 @@
+import streamingGpt4QuestionAnswerTTS from './streamingGpt4QuestionAnswerTTS';
+
 test('streamingGpt4QuestionAnswerTTS correctly', async () => {
-});
+    streamingGpt4QuestionAnswerTTS('What is the meaning of life?').then((result) => {
+        expect(result).toBe('The meaning of life is to be happy.');
+    }
+    );
+}
+);
