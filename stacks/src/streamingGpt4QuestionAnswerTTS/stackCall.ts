6b2abe82c69de44424322f00e69131f4ad6be377
Merwane Hamadi <merwanehamadi@gmail.com>
2023-11-30 14:48:12 -0500

    Move stacks folder

--- stacks/src/streamingGpt4QuestionAnswerTTS/stackCall.ts
+++
@@ -1,4 +0,0 @@
-// stack(
-//     "Call gpt-4 using the streaming endpoint to answer my question and TTS the voice in real time",
-//     {in: "What is the meaning of life ?", out: null // nothing out because I want the content to be spoken directly}
-// )
