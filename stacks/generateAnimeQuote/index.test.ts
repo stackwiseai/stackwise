cf11e42309561b4e687f83ef8d7eebd0214ebba6
ariessanity <ianorillo21@gmail.com>
2023-12-03 01:12:58 +0800

    API: generate random anime quote

--- stacks/generateAnimeQuote/index.test.ts
+++ stacks/generateAnimeQuote/index.test.ts
@@ -3,5 +3,5 @@ import generateAnimateQuote from ".";
 test("Generate a random Anime quote", async () => {
   const response = await generateAnimateQuote();
 
-  expect(response.message).toBeTruthy();
+  expect(response.quote).toBeTruthy();
 });
