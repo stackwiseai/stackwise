8de7f9dd78857f4e7caa61f01abdd2a87833c3ea
Silen Naihin <silen.naihin@gmail.com>
2023-12-02 15:04:09 -0500

    working tests

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,1 @@
-import * as vscode from 'vscode';
-import path from 'path';
-
-export const directoryPath = path.join(
-  vscode.workspace.rootPath,
-  // TODO: pull from config file
-  'stacks'
-);
-
 export const placeholderName = 'stackPlaceholder';
--- src/stack/createStackFile/index.ts
+++ src/stack/createStackFile/index.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import { directoryPath } from '../../constants';
+import { directoryPath } from '../../extension';
 
 export default function createStackFile(
   skeleton: string,
