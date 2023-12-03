2d4ce3c50935971cdb7ae6dddad0ae0aff1aa2a2
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2023-12-03 13:58:16 +0800

    Tests using admin auth supabase

---
+++ stacks/ListAllUsersUsingAdminAuth/index.ts
@@ -0,0 +1,24 @@
+
+import { createClient } from '@supabase/supabase-js'
+require('dotenv').config();
+
+/**
+ * Brief: Retrieve all user of the project using Admin Auth
+ */
+export default async function retrieveAlluser({paginate}:{paginate:any}): Promise<any> {
+    try {
+        const supabaseUrl = String(process.env.SUPABASE_URL)
+        const supabaseServiceRole = String(process.env.SUPABASE_SERVICE_ROLE)
+        const supabase = createClient(supabaseUrl, supabaseServiceRole, {
+            auth: {
+                autoRefreshToken: false,
+                persistSession: false
+            }
+        })
+
+        const { data: { users }, error } = await supabase.auth.admin.listUsers(paginate)
+        return users;
+    } catch (error) {
+        console.error(error);
+    }
+}
