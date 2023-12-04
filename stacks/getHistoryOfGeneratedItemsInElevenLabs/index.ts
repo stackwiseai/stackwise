df86ec19c0fa4e2817dabe50527736391e18b136
pagebook1 <pagebook.kev@gmail.com>
2023-12-04 11:44:07 +0800

    get all generated audio in eleven labs

---
+++ stacks/getHistoryOfGeneratedItemsInElevenLabs/index.ts
@@ -0,0 +1,17 @@
+
+/**
+ * Brief: Get all generated audio in elevenlabs
+ */
+
+type modelType = `${string}/${string}:${string}`
+export default async function getGeneratedAudio(): Promise<any> {
+    try {
+        const apiKey = process.env.ELEVENLABS_API_KEY;
+        const options:RequestInit = {method: 'GET', headers: [['xi-api-key',String(apiKey)]]};
+
+       const response = await fetch('https://api.elevenlabs.io/v1/history', options)
+       return response.json()
+    } catch (error) {
+        console.error(error);
+    }
+}
