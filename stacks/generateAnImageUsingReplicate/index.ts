d813c784bb196a4b11b3aa183d823ec95f41d2c7
pagebook1 <pagebook.kev@gmail.com>
2023-12-04 10:44:42 +0800

    generate image using replicate

---
+++ stacks/generateAnImageUsingReplicate/index.ts
@@ -0,0 +1,21 @@
+import Replicate from "replicate";
+/**
+ * Brief: Generate an image usiung replicate
+ */
+
+type inputType = {
+    prompt: string
+
+}
+type modelType = `${string}/${string}:${string}`
+export default async function generateImageUsingReplicate({ model, input }: { model: modelType, input:inputType }): Promise<any> {
+    try {
+        const replicate = new Replicate({
+            auth: process.env.REPLICATE_API_TOKEN,
+        });
+        const output = await replicate.run(model, { input });
+        return output
+    } catch (error) {
+        console.error(error);
+    }
+}
