8f4ba336a145f248d40267ad4eb4a057aa639338
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2023-12-05 00:05:01 +0800

    remove

--- stacks/getAllAvailableModelsElevenlabs/index.test.ts
+++
@@ -1,6 +0,0 @@
-import getAllAvailableVoices from '.';
-
-test('Get all available models in elevenlabs', async () => {
-  const response = await getAllAvailableVoices();
-  expect(response).toBeTruthy()
-});
