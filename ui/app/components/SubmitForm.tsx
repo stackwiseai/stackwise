0d9463998387e5b7f29987af287dd3b429725b41
Silen Naihin <silen.naihin@gmail.com>
2023-12-10 16:05:08 -0500

    ui pretty much done

--- ui/app/components/SubmitForm.tsx
+++ ui/app/components/SubmitForm.tsx
@@ -5,6 +5,7 @@ interface SubmitFormProps {
   inputValue: string;
   setInputValue: React.Dispatch<React.SetStateAction<string>>;
   loading: boolean;
+  generatedFileContents: string;
 }
 
 const SubmitForm: React.FC<SubmitFormProps> = ({
@@ -12,32 +13,44 @@ const SubmitForm: React.FC<SubmitFormProps> = ({
   inputValue,
   setInputValue,
   loading,
+  generatedFileContents,
 }) => {
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col w-full">
-      <div className="relative w-full">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Ask anything..."
-          className="rounded-full w-full py-2 px-4 border border-gray-400 focus:outline-none focus:shadow-outline"
-          onKeyDown={(e) => {
-            if (e.key === 'Enter')
-              handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
-          }}
-        />
-        <button
-          type="submit"
-          className={`cursor-pointer absolute right-0 top-0 rounded-r-full h-full text-black font-bold px-4 focus:outline-none focus:shadow-outline ${
-            loading ? 'opacity-50 cursor-not-allowed' : ''
-          }`}
-          disabled={loading}
-        >
-          <IoSend />
-        </button>
+    <>
+      <form onSubmit={handleSubmit} className="flex flex-col w-full">
+        <div className="relative w-full">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Ask anything..."
+            className="rounded-full w-full py-2 pl-4 pr-10 border border-gray-400 focus:outline-none focus:shadow-outline"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter')
+                handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+            }}
+          />
+          <button
+            type="submit"
+            className={`cursor-pointer absolute right-0 top-0 rounded-r-full h-full text-black font-bold px-4 focus:outline-none focus:shadow-outline ${
+              loading ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            disabled={loading}
+          >
+            <IoSend />
+          </button>
+        </div>
+      </form>
+      <div className="mt-4 min-h-4 p-4 w-full overflow-auto rounded-md bg-[#faf0e6]">
+        {loading ? (
+          <span className="text-sm text-gray-400">Generating... </span>
+        ) : generatedFileContents ? (
+          generatedFileContents
+        ) : (
+          <p className="text-gray-400 text-sm">Output here...</p>
+        )}
       </div>
-    </form>
+    </>
   );
 };
 
--- ui/app/stacks/chatWithOpenAIStreaming/page.tsx
+++ ui/app/stacks/chatWithOpenAIStreaming/page.tsx
@@ -3,11 +3,12 @@ import SubmitForm from '@/app/components/SubmitForm';
 import ClipboardComponent from '@/app/components/clipboard';
 import React, { useState } from 'react';
 import tw from 'tailwind-styled-components';
+import { FaGithub } from 'react-icons/fa';
 
 // Chat component
 const Chat = () => {
   const [inputValue, setInputValue] = useState('');
-  const [generatedFileContents, setGeneratedFileContents] = useState('');
+  const [generatedFileContents, setGeneratedFileContents] = useState(``);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -56,36 +57,26 @@ const Chat = () => {
   return (
     <Container>
       <TitleContainer>
-        <div className="mb-4 flex">
+        <div className="mb-8 flex items-center justify-between w-full">
           <img className="w-32" src="/stackwise_logo.png" />
-          {/* <div>Github</div> */}
+          <FaGithub className="w-8 h-8" />
         </div>
         <Subtitle>Vercel edge function for OpenAI response streaming.</Subtitle>
       </TitleContainer>
+      <div className="flex mb-4">
+        <span className="text-sm text-gray-500 mt-2">Copy FrontEnd</span>
+        <ClipboardComponent path="/stacks/chatWithOpenAIStreaming/frontend.txt" />
+        <span className="text-sm text-gray-500 mt-2">Copy Backend</span>
+        <ClipboardComponent path="/stacks/chatWithOpenAIStreaming/backend.txt" />
+      </div>
       <MainWrapper>
         <SubmitForm
           handleSubmit={handleSubmit}
           inputValue={inputValue}
           setInputValue={setInputValue}
           loading={loading}
+          generatedFileContents={generatedFileContents}
         />
-        <div className="mt-4 min-h-4 p-4 w-full rounded bg-[#faf0e6]">
-          {loading ? (
-            <span className="text-sm text-gray-400">
-              Might take a minute or 2 ...{' '}
-            </span>
-          ) : generatedFileContents ? (
-            generatedFileContents
-          ) : (
-            <p className="text-gray-400 text-sm">Output here...</p>
-          )}
-        </div>
-        <>
-          <span className="text-sm text-gray-500 mt-2">Copy FrontEnd</span>
-          <ClipboardComponent path="/stacks/chatWithOpenAIStreaming/page.tsx" />
-          <span className="text-sm text-gray-500 mt-2">Copy Backend</span>
-          <ClipboardComponent path="/stacks/chatWithOpenAIStreaming/route.ts" />
-        </>
       </MainWrapper>
     </Container>
   );
@@ -103,10 +94,13 @@ const Container = tw.div`
 
 const TitleContainer = tw.div`
   text-center
-  mb-8
   flex
   flex-col
   items-center
+  h-[45%]
+  justify-end
+  mb-4
+  w-1/2
 `;
 const Subtitle = tw.p`
   text-lg
@@ -116,6 +110,6 @@ const MainWrapper = tw.div`
   w-1/2
   flex
   flex-col
-  justify-center
   items-center
+  h-[55%]
 `;
