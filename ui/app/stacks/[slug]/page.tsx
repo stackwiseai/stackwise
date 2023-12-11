9591ac6cd1968e797a723d180a164b210cb111ea
Silen Naihin <silen.naihin@gmail.com>
2023-12-10 19:29:59 -0500

    code view

--- ui/app/stacks/[slug]/page.tsx
+++ ui/app/stacks/[slug]/page.tsx
@@ -12,6 +12,10 @@ import dynamic from 'next/dynamic';
 import { stackDB } from '../stackDB';
 
 const Chat = ({ params }: { params: { slug: string } }) => {
+  const [showFrontendCode, setShowFrontendCode] = useState<boolean>(false);
+  const [backendCode, setBackendCode] = useState<string>('');
+  const [frontendCode, setFrontendCode] = useState<string>('');
+  const [dropdownSelection, setDropdownSelection] = useState<string>('Usage');
   const stackUuid = params.slug ?? null;
   const stackName = stackDB[`${stackUuid}`].name;
 
@@ -25,120 +29,113 @@ const Chat = ({ params }: { params: { slug: string } }) => {
     }
   );
 
-  const Chat = () => {
-    const [showFrontendCode, setShowFrontendCode] = useState<boolean>(false);
-    const [backendCode, setBackendCode] = useState<string>('');
-    const [frontendCode, setFrontendCode] = useState<string>('');
-    const [dropdownSelection, setDropdownSelection] = useState<string>('Usage');
+  const getPathText = async (path: string) => {
+    const response = await fetch(path);
+    const data = await response.text();
+    return data;
+  };
 
-    const getPathText = async (path: string) => {
-      const response = await fetch(path);
-      const data = await response.text();
-      return data;
-    };
+  const frontendPath = '/stacks/chatWithOpenAIStreaming/frontend.txt';
+  const backendPath = '/stacks/chatWithOpenAIStreaming/backend.txt';
 
-    const frontendPath = '/stacks/chatWithOpenAIStreaming/frontend.txt';
-    const backendPath = '/stacks/chatWithOpenAIStreaming/backend.txt';
+  useEffect(() => {
+    getPathText(frontendPath).then((data) => setFrontendCode(data));
+    getPathText(backendPath).then((data) => setBackendCode(data));
+  }, []);
 
-    useEffect(() => {
-      getPathText(frontendPath).then((data) => setFrontendCode(data));
-      getPathText(backendPath).then((data) => setBackendCode(data));
-    }, []);
-
-    return (
-      <Container>
-        <Link
-          className="cursor-pointer fixed bottom-4 right-4"
-          href="https://github.com/stackwiseai/stackwise"
-          target="_blank"
-        >
-          <IoLogoGithub className="w-8 h-8" />
-        </Link>
-        <Link
-          className="cursor-pointer fixed bottom-4 left-4"
-          href="mailto:silen@stackwise.ai"
-        >
-          Not happy with the code? <br></br>
-          Email <b>contact@stackwise.ai</b>
-        </Link>
-        <TitleContainer>
-          <div className="w-full mb-4 flex justify-center">
-            <img className="w-32" src="/stackwise_logo.png" />
-          </div>
-          <Subtitle>stackIdToName.stackUuid.description</Subtitle>
-        </TitleContainer>
-        <div className="flex items-center space-x-6">
-          <button
-            onClick={() => {
-              setDropdownSelection(
-                dropdownSelection === 'Usage' ? 'Code' : 'Usage'
-              );
-            }}
-            className="border rounded-md px-2 py-1 pr-2 flex items-center space-x-2"
-          >
-            <p>{dropdownSelection === 'Usage' ? 'Code' : 'Usage'}</p>
-            {dropdownSelection === 'Usage' ? <FaCode /> : <MdOutlineInput />}
-          </button>
-          <ClipboardComponent
-            title={
-              <>
-                Copy <b className="text-black">Frontend</b>
-              </>
-            }
-            path={frontendPath}
-          />
-          <ClipboardComponent
-            title={
-              <>
-                Copy <b className="text-black">Backend</b>
-              </>
-            }
-            path={backendPath}
-          />
+  return (
+    <Container>
+      <Link
+        className="cursor-pointer fixed bottom-4 right-4"
+        href="https://github.com/stackwiseai/stackwise"
+        target="_blank"
+      >
+        <IoLogoGithub className="w-8 h-8" />
+      </Link>
+      <Link
+        className="cursor-pointer fixed bottom-4 left-4"
+        href="mailto:silen@stackwise.ai"
+      >
+        Not happy with the code? <br></br>
+        Email <b>contact@stackwise.ai</b>
+      </Link>
+      <TitleContainer>
+        <div className="w-full mb-4 flex justify-center">
+          <img className="w-32" src="/stackwise_logo.png" />
         </div>
-        <MainWrapper>
-          {dropdownSelection === 'Code' ? (
-            <div className="bg-[#1e1e1e] rounded-md w-full">
-              <div className="text-white flex items-center w-full">
-                <button
-                  onClick={() => setShowFrontendCode(true)}
-                  className={`border-r border-b p-2 ${
-                    showFrontendCode && 'border-b-red-400 border-b-2'
-                  }`}
-                >
-                  frontend code
-                </button>
-                <button
-                  onClick={() => setShowFrontendCode(false)}
-                  className={`border-r border-b p-2 rounded-br-lg ${
-                    !showFrontendCode && 'border-b-red-400 border-b-2'
-                  }`}
-                >
-                  backend code
-                </button>
-              </div>
-              {/* <pre className="min-h-4 p-3 max-h-96 w-full overflow-auto text-gray-200 text-sm whitespace-pre-wrap break-all">
+        <Subtitle>stackIdToName.stackUuid.description</Subtitle>
+      </TitleContainer>
+      <div className="flex items-center space-x-6">
+        <button
+          onClick={() => {
+            setDropdownSelection(
+              dropdownSelection === 'Usage' ? 'Code' : 'Usage'
+            );
+          }}
+          className="border rounded-md px-2 py-1 pr-2 flex items-center space-x-2"
+        >
+          <p>{dropdownSelection === 'Usage' ? 'Code' : 'Usage'}</p>
+          {dropdownSelection === 'Usage' ? <FaCode /> : <MdOutlineInput />}
+        </button>
+        <ClipboardComponent
+          title={
+            <>
+              Copy <b className="text-black">Frontend</b>
+            </>
+          }
+          path={frontendPath}
+        />
+        <ClipboardComponent
+          title={
+            <>
+              Copy <b className="text-black">Backend</b>
+            </>
+          }
+          path={backendPath}
+        />
+      </div>
+      <MainWrapper>
+        {dropdownSelection === 'Code' ? (
+          <div className="bg-[#1e1e1e] rounded-md w-full">
+            <div className="text-white flex items-center w-full">
+              <button
+                onClick={() => setShowFrontendCode(true)}
+                className={`border-r border-b p-2 ${
+                  showFrontendCode && 'border-b-red-400 border-b-2'
+                }`}
+              >
+                frontend code
+              </button>
+              <button
+                onClick={() => setShowFrontendCode(false)}
+                className={`border-r border-b p-2 rounded-br-lg ${
+                  !showFrontendCode && 'border-b-red-400 border-b-2'
+                }`}
+              >
+                backend code
+              </button>
+            </div>
+            {/* <pre className="min-h-4 p-3 max-h-96 w-full overflow-auto text-gray-200 text-sm whitespace-pre-wrap break-all">
               {showFrontendCode ? frontendCode : backendCode}
             </pre> */}
-              <SyntaxHighlighter
-                language="javascript"
-                className="min-h-4 p-3 max-h-96 w-full overflow-auto overflow-y-hidden text-gray-200 text-sm whitespace-pre-wrap break-all"
-                style={vscDarkPlus}
-                lineProps={{
-                  style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' },
-                }}
-                wrapLines={true}
-              >
-                {showFrontendCode ? frontendCode : backendCode}
-              </SyntaxHighlighter>
-            </div>
-          ) : (
-            <DynamicComponent />
-          )}
-        </MainWrapper>
-      </Container>
-    );
-  };
+            <SyntaxHighlighter
+              language="javascript"
+              className="min-h-4 p-3 max-h-96 w-full overflow-auto overflow-y-hidden text-gray-200 text-sm whitespace-pre-wrap break-all"
+              style={vscDarkPlus}
+              lineProps={{
+                style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' },
+              }}
+              wrapLines={true}
+            >
+              {showFrontendCode ? frontendCode : backendCode}
+            </SyntaxHighlighter>
+          </div>
+        ) : (
+          <DynamicComponent />
+        )}
+      </MainWrapper>
+    </Container>
+  );
 };
 
 export default Chat;
--- ui/app/stacks/stackDB.ts
+++ ui/app/stacks/stackDB.ts
@@ -1,11 +1,11 @@
 export const stackDB = {
   '4f62be3a3b464f8e85d79e3d7e3c9b67': {
     name: 'ChatWithOpenAIStreaming',
-    description: 'TODO',
+    description: 'Vercel edge function for OpenAI response streaming.',
   },
   '1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d': {
     name: 'ElevenLabsTTS',
-    description: 'Vercel edge function for OpenAI response streaming.',
+    description: 'TODO',
   },
   '9876543210fedcba54321098f7e6d5c4': {
     name: 'GetImageDescriptionOpenAI',
