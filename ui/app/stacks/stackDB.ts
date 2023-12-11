a7ea19ffd61371d551a05c354a9f30f273d3b52d
Merwane Hamadi <merwanehamadi@gmail.com>
2023-12-10 20:23:00 -0500

    push stackDB

--- ui/app/stacks/stackDB.ts
+++ ui/app/stacks/stackDB.ts
@@ -1,21 +1,22 @@
 export const stackDB = {
-  '4f62be3a3b464f8e85d79e3d7e3c9b67': {
+  gbKm2SKi484: {
     name: 'ChatWithOpenAIStreaming',
     description: 'Vercel edge function for OpenAI response streaming.',
   },
-  '1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d': {
+  //base 64 like above
+  aEKmWSKi395: {
     name: 'ElevenLabsTTS',
     description: 'TODO',
   },
-  '9876543210fedcba54321098f7e6d5c4': {
+  fedcba54321: {
     name: 'GetImageDescriptionOpenAI',
     description: 'TODO',
   },
-  abc123456789def0123456789abcdef0: {
+  abc12345678: {
     name: 'UseOpenAIAssistant',
     description: 'TODO',
   },
-  cda123456739def0123456783abc5ef0: {
+  cda123456739: {
     name: 'CreateAICanvas',
     description: 'TODO',
   },
