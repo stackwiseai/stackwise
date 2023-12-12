ef1a30b5d588b3f1a8e9053b6cacee7c84b82a04
Silen Naihin <silen.naihin@gmail.com>
2023-12-11 21:48:19 -0500

    adjusting to 7 chars

--- ui/app/stacks/stackDB.ts
+++ ui/app/stacks/stackDB.ts
@@ -1,22 +1,22 @@
 export const stackDB = {
-  gbKm2SKi484: {
+  gbKm2SK: {
     name: 'ChatWithOpenAIStreaming',
     description: 'Vercel edge function for OpenAI response streaming.',
   },
   //base 64 like above
-  aEKmWSKi395: {
+  aEKmWSK: {
     name: 'ElevenLabsTTS',
     description: 'TODO',
   },
-  fedcba54321: {
+  fedcba5: {
     name: 'GetImageDescriptionOpenAI',
     description: 'TODO',
   },
-  abc12345678: {
+  abc1234: {
     name: 'UseOpenAIAssistant',
     description: 'TODO',
   },
-  blKmsSKi31a: {
+  blKfsSK: {
     name: 'CreateAICanvas',
     description:
       'Real time image rendering using Fal. Draw on the left canvas and add a prompt.',
