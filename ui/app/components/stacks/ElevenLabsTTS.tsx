92b656edd3249ef814d56db517a333df8f1b433c
Merwane Hamadi <merwanehamadi@gmail.com>
2023-12-12 08:06:20 -0500

    polish elevenlabs

--- ui/app/components/stacks/ElevenLabsTTS.tsx
+++ ui/app/components/stacks/ElevenLabsTTS.tsx
@@ -1,6 +1,7 @@
 'use client';
 import ClipboardComponent from '@/app/components/clipboard';
 import React, { useState } from 'react';
+import { IoSend } from 'react-icons/io5';
 
 // Chat component
 const Chat = () => {
@@ -18,36 +19,31 @@ const Chat = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <div className="w-2/3 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <div className="relative w-full">
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              placeholder="Type here..."
-              className="rounded-full w-full py-4 pl-4 pr-10 border border-gray-400 focus:outline-none focus:shadow-outline text-xl"
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  handleSubmit(
-                    e as unknown as React.FormEvent<HTMLFormElement>
-                  );
-                }
-              }}
-            />
-          </div>
-          <div className="mt-4 min-h-4 p-4 w-full overflow-auto rounded-md bg-[#faf0e6]">
-            {loading ? (
-              <span className="text-sm text-gray-400">Generating... </span>
-            ) : generatedFileContents ? (
-              generatedFileContents
-            ) : (
-              <p className="text-gray-400 text-sm">Output here...</p>
-            )}
-          </div>
-        </form>
-      </div>
+    <div className="w-2/3">
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <div className="relative w-full">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Ask anything..."
+            className="rounded-full w-full py-2 pl-4 pr-10 border border-gray-400 focus:outline-none focus:shadow-outline"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter')
+                handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+            }}
+          />
+          <button
+            type="submit"
+            className={`cursor-pointer absolute right-0 top-0 rounded-r-full h-full text-black font-bold px-4 focus:outline-none focus:shadow-outline ${
+              loading ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            disabled={loading}
+          >
+            <IoSend />
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
