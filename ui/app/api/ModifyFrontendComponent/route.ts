e26ebe5acdbfc07898cd15d8ed584c02efb7364c
Merwane Hamadi <merwanehamadi@gmail.com>
2023-12-13 08:13:53 -0500

    modify edge runtime syntax

--- ui/app/api/ModifyFrontendComponent/route.ts
+++ ui/app/api/ModifyFrontendComponent/route.ts
@@ -14,9 +14,7 @@ const openai = new OpenAI({
   },
 });
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
 
 export async function POST(req: Request) {
   // if (req.method !== 'POST') {
