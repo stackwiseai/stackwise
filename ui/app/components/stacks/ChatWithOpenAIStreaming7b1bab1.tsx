ff5c1f7a8c993d18f51cfbbb26ad3553faf9dd73
stackwisebot <152521574+stackwisebot@users.noreply.github.com>
2023-12-13 17:11:11 -0500

    Building ChatWithOpenAIStreaming7b1bab1

---
+++ ui/app/components/stacks/ChatWithOpenAIStreaming7b1bab1.tsx
@@ -0,0 +1,12 @@
+// use client
+import { NextPage } from 'next';
+
+const HelloWorld: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
