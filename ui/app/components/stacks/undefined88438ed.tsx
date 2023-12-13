204da92051f47edd7d3288d6a948d5f647b17f03
stackwisebot <152521574+stackwisebot@users.noreply.github.com>
2023-12-13 16:57:04 -0500

    Building undefined88438ed

---
+++ ui/app/components/stacks/undefined88438ed.tsx
@@ -0,0 +1,12 @@
+// use client
+import { NextPage } from 'next';
+
+const HelloWorld: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello, World! Please</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
