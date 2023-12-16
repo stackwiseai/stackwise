57ec6e0a54ca801124cfcbb54e6714f105825be3
Silen Naihin <silen.naihin@gmail.com>
2023-12-16 18:28:24 -0500

    lambda function deployment working :)

--- npm_package/exec/index.ts
+++ npm_package/exec/index.ts
@@ -3,9 +3,10 @@
 import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
-import { vol } from 'memfs';
-import archiver from 'archiver';
 import transformRoute from './transformRoute.js';
+
+// Define __dirname in ES module
+const __dirname = new URL('.', import.meta.url).pathname;
 
 async function packageAndDeployLambda(routePath: string): Promise<void> {
   // Determine the base directory of route.ts and create 'lambda' subdirectory
@@ -20,21 +21,34 @@ async function packageAndDeployLambda(routePath: string): Promise<void> {
   const originalContent = fs.readFileSync(routeDir, 'utf8');
   const transformedContent = await transformRoute(originalContent);
 
-  // Use an in-memory file system for the transformed file
-  vol.writeFileSync('/lambda.js', transformedContent);
+  // Initialize npm and install packages
+  execSync('npm init -y', { cwd: lambdaDir, stdio: 'inherit' });
+  const packages = ['openai']; // Add other packages as needed
+  if (packages.length > 0) {
+    execSync(`npm install ${packages.join(' ')}`, {
+      cwd: lambdaDir,
+      stdio: 'inherit',
+    });
+  }
 
-  // Create a zip archive in memory and write it to the 'lambda' subdirectory
-  let buffer: Buffer = Buffer.from([]);
-  const archive = archiver('zip', { zlib: { level: 9 } }).on(
-    'data',
-    data => (buffer = Buffer.concat([buffer, data as Buffer]))
-  );
+  // Write the transformed file to the lambda directory
+  fs.writeFileSync(path.join(lambdaDir, 'index.js'), transformedContent);
 
-  archive.append(vol.createReadStream('/lambda.js'), { name: 'lambda.js' });
-  await archive.finalize();
+  // Copy the lambda.tf file to the lambda directory
+  const terraformFilePath = path.join(__dirname, 'lambda.tf');
+  fs.copyFileSync(terraformFilePath, path.join(lambdaDir, 'lambda.tf'));
 
+  // Create the utils
+  const utilsFilePath = path.join(__dirname, 'utils', 'check_iam_role.sh');
+  const utilsDir = path.join(lambdaDir, 'utils');
+  if (!fs.existsSync(utilsDir)) {
+    fs.mkdirSync(utilsDir);
+  }
+  fs.copyFileSync(utilsFilePath, path.join(utilsDir, 'check_iam_role.sh'));
+
+  // Create the zip file
   const zipFilePath = path.join(lambdaDir, 'function.zip');
-  fs.writeFileSync(zipFilePath, buffer);
+  execSync(`zip -r ${zipFilePath} .`, { cwd: lambdaDir, stdio: 'inherit' });
 
   // Execute Terraform commands within the 'lambda' subdirectory
   execSync('terraform init', { cwd: lambdaDir, stdio: 'inherit' });
