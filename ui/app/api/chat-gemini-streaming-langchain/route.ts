cba5cc19d7b4a3af8740789ffb372c0447bb41af
Merwane Hamadi <merwanehamadi@gmail.com>
2023-12-17 12:36:01 -0500

    Chat gemini streaming langchain

--- ui/app/api/chat-gemini-streaming-langchain/route.ts
+++ ui/app/api/chat-gemini-streaming-langchain/route.ts
@@ -1,6 +1,30 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
 export async function POST(req: Request) {
-  const { input } = await req.json();
-  return new Response(
-    JSON.stringify({ output: `You sent this message to the server: ${input}` })
-  );
+  const chat = new ChatGoogleGenerativeAI();
+  const { messages } = await req.json();
+
+  try {
+    const textResponse = await chat.invoke([["human", messages]]);
+    const stream = await chat.stream([
+      ["human", "Tell me a joke about bears."],
+    ]);
+
+    for await (const chunk of stream) {
+      console.log(chunk);
+    }
+    const output = textResponse.content; // assuming the text you want is in `content`
+
+    const responseJson = JSON.stringify({ output });
+
+    return new Response(responseJson, {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: "An error occurred" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 }
