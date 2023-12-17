85a5db46c75de7d78e7f3b0bcbc37ddf751f7cc1
stackwisebot <152521574+stackwisebot@users.noreply.github.com>
2023-12-17 11:24:54 -0500

    Stack test-stack-creation-12324 created

---
+++ ui/app/api/test-stack-creation-12324/route.ts
@@ -0,0 +1,6 @@
+export async function POST(req: Request) {
+  const { input } = await req.json();
+  return new Response(
+    JSON.stringify({ output: `You sent this message to the server: ${input}` })
+  );
+}
