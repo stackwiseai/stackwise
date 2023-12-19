77b3090609e30761e96a1da2e1a77a918d4ab855
Silen Naihin <silen.naihin@gmail.com>
2023-12-19 12:01:50 -0500

    existing lambda function, terraform template

--- npm_package/exec/getLambdaFunctionArn.ts
+++ npm_package/exec/getLambdaFunctionArn.ts
@@ -1,13 +1,13 @@
 import { execSync } from 'child_process';
 
-export function getLambdaFunctionArn(functionName) {
+export function getLambdaFunctionArn(functionName: string, region: string) {
   try {
-    const awsCliCommand = `aws lambda get-function --function-name ${functionName}`;
+    const awsCliCommand = `aws lambda get-function --function-name ${functionName} --region ${region} --profile stackwise-agent`;
     const output = execSync(awsCliCommand).toString();
     const functionData = JSON.parse(output);
     return functionData.Configuration.FunctionArn;
   } catch (error) {
     console.error('Error retrieving Lambda function ARN:', error);
-    return null; // or handle the error as needed
+    return null;
   }
 }
