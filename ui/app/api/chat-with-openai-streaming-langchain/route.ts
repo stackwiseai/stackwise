b5213253ebe9a42bac10a8502e462a62e57d6015
Kushagra <skushagra.sharma@gmail.com>
2023-12-20 19:51:29 +0530

    Update route.ts

--- ui/app/api/chat-with-openai-streaming-langchain/route.ts
+++ ui/app/api/chat-with-openai-streaming-langchain/route.ts
@@ -1,23 +1,46 @@
-import { OpenAI } from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { NextRequest, NextResponse } from "next/server";
+import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
 
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY!})
-export const runtime = 'edge'
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BytesOutputParser } from "langchain/schema/output_parser";
+import { PromptTemplate } from "langchain/prompts";
 
-export async function POST(req: Request) {
-  
-  const { messages } = await req.json();
+export const runtime = "edge";
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: messages,
-    max_tokens: 500,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 1,
-    presence_penalty: 1,
-  })
-  const stream = OpenAIStream(response)
-  return new StreamingTextResponse(stream)
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
+
+const TEMPLATE = `
+Current conversation:
+{chat_history}
+
+User: {input}
+AI:`;
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const messages = body.messages ?? [];
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+    const currentMessageContent = messages[messages.length - 1].content;
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+   
+   
+    const model = new ChatOpenAI({
+      temperature: 0.8,
+    });
+   
+    const outputParser = new BytesOutputParser();
+    const chain = prompt.pipe(model).pipe(outputParser);
+
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join("\n"),
+      input: currentMessageContent,
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
 }
