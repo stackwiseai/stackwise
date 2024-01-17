a22007f3df6a87af7644e66a34e9938ee076843a
Silen Naihin <silen.naihin@gmail.com>
2024-01-16 21:46:03 -0500

    ui changes working

--- ui/app/components/stacks/cover-image-and-subtitle.tsx
+++ ui/app/components/stacks/cover-image-and-subtitle.tsx
@@ -5,8 +5,10 @@ import { useState } from 'react';
 export const GenerateImageAndSubtitle = () => {
   const [video, setVideo] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
+  const [summary, setSummary] = useState('');
   const [subtitle, setSubtitle] = useState('');
   const [imgUrl, setImgUrl] = useState('');
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleVideoUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (subtitle) {
@@ -45,6 +47,7 @@ export const GenerateImageAndSubtitle = () => {
       const data = await response.json();
       setSubtitle(data.subtitle);
       setImgUrl(data.imgUrl);
+      setSummary(data.summarizedText);
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -93,7 +96,7 @@ export const GenerateImageAndSubtitle = () => {
           )}
         </label>
       </div>
-      <div className="flex w-full items-center justify-center space-x-4">
+      <div className="flex w-full flex-col items-center justify-center space-x-4 sm:flex-row">
         {video && (
           <video
             src={URL.createObjectURL(video)}
@@ -102,21 +105,56 @@ export const GenerateImageAndSubtitle = () => {
           />
         )}
         {subtitle && (
-          <div className="w-1/2 space-y-3 px-6">
-            <div className="mb-3 text-sm">
-              {imgUrl && <img src={imgUrl} />}
-              <div className="text-zinc-500">Generated Image</div>
-            </div>
-
-            <div>
+          <div className="mt-3 flex w-full flex-col items-center justify-center space-y-3 px-3 sm:mt-0 sm:w-1/2">
+            <div className="text-sm">{imgUrl && <img src={imgUrl} />}</div>
+            {summary && (
+              <div className="mb-3 text-sm">
+                <span className="font-medium">Summary of video:</span> {summary}
+              </div>
+            )}
+            <div className="flex w-full flex-col justify-center space-y-2">
               <button
-                className="rounded-md border border-black px-3 py-1 font-medium"
-                onClick={() => downloadSubtitle()}
+                className="mx-auto rounded-md border border-black px-3 py-1 font-medium xl:w-1/2"
+                onClick={downloadSubtitle}
               >
-                Download Subtitle
+                Download Subtitles
               </button>
-              <div className="mt-2 text-clip text-sm text-zinc-500">
-                Click the download button to start downloading the subtitle
+              <div
+                className="mx-auto flex cursor-pointer items-center justify-center space-x-2"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              >
+                <svg
+                  className={`h-4 w-4 ${isDropdownOpen ? 'rotate-180' : ''}`}
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+                <span className="text-center text-sm font-medium">
+                  {isDropdownOpen ? 'Hide Subtitles' : 'Show Subtitles'}
+                </span>
+              </div>
+
+              <div
+                className="w-full"
+                style={{
+                  maxHeight: isDropdownOpen ? '500px' : '0',
+                  overflow: 'hidden',
+                  transition: 'max-height 0.3s ease-in-out',
+                }}
+              >
+                {isDropdownOpen && (
+                  <div className="bg-gray-100 p-2 text-sm">
+                    {subtitle || 'No Subtitles'}
+                  </div>
+                )}
               </div>
             </div>
           </div>
