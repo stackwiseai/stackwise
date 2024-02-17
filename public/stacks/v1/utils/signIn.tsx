72f88a1ce58f91b65b8f91c505fdac30931c8eac
Silen Naihin <silen.naihin@gmail.com>
2024-02-17 14:01:12 -0500

    shortening amount of files

--- public/stacks/v1/utils/signIn.tsx
+++ public/stacks/v1/utils/signIn.tsx
@@ -1,24 +1,26 @@
-"use client";
+'use client';
 
-import { supabaseClient } from "./stack-db";
-
+import { supabaseClient } from './stack-db';
 
 export default function signIn() {
- 
+  const baseUrl =
+    process.env.NODE_ENV === 'production'
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` // Use Vercel's environment variable in production
+      : 'http://localhost:3000'; // Default to localhost in development
+
   async function signInWithGithub() {
-    
     const { data, error } = await supabaseClient.auth.signInWithOAuth({
-      provider: "github",
+      provider: 'github',
       options: {
         scopes: 'public_repo',
-        redirectTo: `${process.env.NEXT_PUBLIC_URL}/stacks/create-stack-boilerplate`
+        redirectTo: `${baseUrl}/stacks/create-stack-boilerplate`,
       },
     });
   }
   return (
-    <button 
+    <button
       onClick={signInWithGithub}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
     >
       Sign in with Github
     </button>
